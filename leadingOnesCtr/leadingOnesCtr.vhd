8f2e521e05a82aa5aad6ed8fcecc6f9bfe63008b
kogaadam <43ada21@gmail.com>
2020-04-24 18:29:52 -0400

    added outline and generics

--- leadingOnesCtr/leadingOnesCtr.vhd
+++ leadingOnesCtr/leadingOnesCtr.vhd
@@ -1,1 +1,17 @@
 library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
+
+entity leadingOnesCtr is
+	generic (
+		BITS_IN: positive := 8;
+		BITS_OUT: positive := 4);	--calculated by user as ceil(log2(BITS_IN+1))
+	port (
+		inp_vector: in std_logic_vector(BITS_IN-1 downto 0));
+end entity;
+
+
+architecture rtl of leadingOnesCtr is
+begin
+end architecture;
