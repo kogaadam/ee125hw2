5d64fbae859d66e25d8369fc75c27987e8316951
kogaadam <43ada21@gmail.com>
2020-04-25 13:10:01 -0400

    added ssd interfacing

--- leadingOnesCtr/leadingOnesCtr.vhd
+++ leadingOnesCtr/leadingOnesCtr.vhd
@@ -8,7 +8,8 @@ entity leadingOnesCtr is
 		BITS_OUT: positive := 4);	--calculated by user as ceil(log2(BITS_IN+1))
 	port (
 		inp_vector: in std_logic_vector(BITS_IN-1 downto 0);
-		onesCount: out std_logic_vector(BITS_OUT-1 downto 0));
+		onesCount: out std_logic_vector(BITS_OUT-1 downto 0);
+		ssd: out std_logic_vector(6 downto 0));
 end entity;
 
 
@@ -29,5 +30,17 @@ begin
 	end generate;
 	
 	onesCount <= std_logic_vector(to_unsigned(internalOnesCt(BITS_IN), BITS_OUT));
+	
+	with onesCount select
+		ssd <= "0000001" when "0000",
+				 "1001111" when "0001",
+				 "0010010" when "0010",
+				 "0000110" when "0011",
+				 "1001100" when "0100",
+				 "0100100" when "0101",
+				 "0100000" when "0110",
+				 "0001111" when "0111",
+				 "0000000" when "1000",
+				 "1111110" when others;
 	
 end architecture;
