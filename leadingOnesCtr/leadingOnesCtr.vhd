087353330916b4abf32a1156a043d93a417c9630
kogaadam <43ada21@gmail.com>
2020-04-26 01:27:00 -0400

    changed var names

--- leadingOnesCtr/leadingOnesCtr.vhd
+++ leadingOnesCtr/leadingOnesCtr.vhd
@@ -15,21 +15,21 @@ end entity;
 
 architecture concurrent of leadingOnesCtr is
 	type integer_array is array (0 to BITS_IN) of integer range 0 to BITS_IN;
-	signal internalOnesCt: integer_array;
-	signal internalOnes: std_logic_vector(BITS_IN downto 0);
+	signal internalLeadOnesCt: integer_array;
+	signal internalLeadOnes: std_logic_vector(BITS_IN downto 0);
 begin
-	internalOnesCt(0) <= 0;
-	internalOnes(BITS_IN) <= '1';
+	internalLeadOnesCt(0) <= 0;
+	internalLeadOnes(BITS_IN) <= '1';
 	
 	gen: for i in 0 to BITS_IN-1 generate
-		internalOnes(BITS_IN - 1 - i) <= '1' when inp_vector(BITS_IN - 1 - i) and internalOnes(BITS_IN - i) else '0';
+		internalLeadOnes(BITS_IN - 1 - i) <= '1' when inp_vector(BITS_IN - 1 - i) and internalLeadOnes(BITS_IN - i) else '0';
 	end generate;
 		
 	gen2: for i in 1 to BITS_IN generate
-		internalOnesCt(i) <= internalOnesCt(i-1) + 1 when internalOnes(i-1) else internalOnesCt(i-1);
+		internalLeadOnesCt(i) <= internalLeadOnesCt(i-1) + 1 when internalLeadOnes(i-1) else internalLeadOnesCt(i-1);
 	end generate;
 	
-	onesCount <= std_logic_vector(to_unsigned(internalOnesCt(BITS_IN), BITS_OUT));
+	onesCount <= std_logic_vector(to_unsigned(internalLeadOnesCt(BITS_IN), BITS_OUT));
 	
 	with onesCount select
 		ssd <= "0000001" when "0000",
